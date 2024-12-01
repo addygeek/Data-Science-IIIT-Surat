0cb506dba65b786af6c4fabab62b1082aef14480
addygeek <ui22cs03@iiitsurat.ac.in>
2024-12-01 23:02:52 +0530

    t stats

---
+++ EXTRAS/t_test.py
@@ -0,0 +1,5 @@
+from scipy.stats import ttest_1samp
+
+data = [10, 12, 14, 16, 18]
+t_stat, p_value = ttest_1samp(data, popmean=15)  # Test mean=15
+print("t-statistic:", t_stat, "p-value:", p_value)

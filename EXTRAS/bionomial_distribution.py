4ec7b305187cbdd2a755be3e02c346388b50ebdf
Aditya Kumar <ui22cs03@iiitsurat.ac.in>
2025-01-02 23:59:32 +0530

    predictive analysis start

--- EXTRAS/bionomial_distribution.py
+++ EXTRAS/bionomial_distribution.py
@@ -5,5 +5,5 @@ n, p = 10, 0.5  # Trials, Probability of success
 x = np.arange(0, n+1)
 pmf = binom.pmf(x, n, p)
 plt.bar(x, pmf)
-plt.title("Binomial Distribution")
+plt.title("Binomial Distributions")
 plt.show()
